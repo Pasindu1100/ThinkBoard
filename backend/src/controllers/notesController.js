8ce1175d08edc7f4295cedd561af93edf2d24d38
Pasindu1100 <pasinduraanjana@gmail.com>
2026-02-11 18:18:02 +0530

    delete and update api done

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -31,14 +31,18 @@ export async function updateNote(req, res) {
     try{
         const {title, content} = req.body;
         const updatedNote = await Note.findByIdAndUpdate(req.params.id, {title, content},{
-            new: true, // return the updated document
+            //new: true, // return the updated document
+            returnDocument: "after", // return the updated document (Mongoose 6.0+)
             runValidators: true, // run schema validators on update
             
+
         });
 
         if(!updatedNote){
             return res.status(404).json({message:"Note not found"});
         }
+
+        res.status(200).json({message:"Note updated successfully", note: updatedNote});
     }catch(error){
          console.error("Error in updateNote controller:", error);
         res.status(500).json({message:"internal server error"});
@@ -47,5 +51,14 @@ export async function updateNote(req, res) {
 
 
 export async function deleteNote(req, res) {
-     res.status(200).json({message:"Note deleted successfully"});
+    try{
+        const deletedNote = await Note.findByIdAndDelete(req.params.id);
+        if(!deletedNote){
+            return res.status(404).json({message:"Note not found"});
+        }
+        res.status(200).json({message:"Note deleted successfully"});
+    }catch(error){
+         console.error("Error in deleteNote controller:", error);
+        res.status(500).json({message:"internal server error"});
+    }
 }   
