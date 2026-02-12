462964f168076e7724ef322b16251276e0f1127b
Pasindu1100 <pasinduraanjana@gmail.com>
2026-02-12 10:15:17 +0530

    db starts before server

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,7 @@ import rateLimiter from "./middleware/rateLimiter.js";
 // Load .env variables
 dotenv.config();
 
-// connect to database
-connectDB();
+
 
 const app = express();
 // Middleware to parse JSON bodies
@@ -19,13 +18,21 @@ app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
 
 
-// Start server
 
-// Use PORT from environment variables or default to 5001
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+
+// best practice is to connect to the database before starting the server, so that if there are any issues with the database connection, the server won't start and we can handle the error appropriately.
+// connect to database
+
+
+connectDB().then(() => {
+  // Start server
+  // Use PORT from environment variables or default to 5001
+  const PORT = process.env.PORT || 5001;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+    });
 });
+
 
 
 // app.listen(5001,() => {
