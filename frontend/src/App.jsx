16c2dd8d0198bb9a412d60f767108f6af620f315
Pasindu1100 <pasinduraanjana@gmail.com>
2026-02-12 12:04:00 +0530

    Nav bar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,7 @@ import './index.css';
 
 export const App = () => {
   return (
-    <div>
-      <button className="btn btn-primary">Click Me Please</button>
-      <button onClick={() => toast.success("congrats!!")} className="bg-red-400 p-4">Click me </button>
-      <div className="p-4 bg-blue-500">Hello</div>
+    <div data-theme="forest" >
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
