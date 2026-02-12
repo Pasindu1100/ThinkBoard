f7ac6aab707e59dbb75f8bce134b4615c32a834d
Pasindu1100 <pasinduraanjana@gmail.com>
2026-02-12 11:11:32 +0530

    tailwindcss configuration and basic page components for ThinkBoard

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import {Route, Routes } from "react-router"
+
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import toast from "react-hot-toast"
+import './index.css';
+
+export const App = () => {
+  return (
+    <div>
+      <button onClick={() => toast.success("congrats!!")} className="bg-red-400 p-4">Click me </button>
+      <div className="p-4 bg-blue-500">Hello</div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+
+    </div>
+  )
+}
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import './index.css';
+
+import { BrowserRouter } from 'react-router'
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Toaster/>
+    </BrowserRouter>
   </StrictMode>,
+  
 )
